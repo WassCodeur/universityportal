aecf98c5d13f13f07d33982e6e5cd9aabdd75d26
WassCodeur <wachioubouraima56@gmail.com>
2024-07-29 17:22:29 +0000

    chore: Update Sphinx documentation configuration and theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 pygments_style = "sphinx"
-html_theme = "pydata_sphinx_theme"
+html_theme = "furo"
 
 html_static_path = ['_static']
 
